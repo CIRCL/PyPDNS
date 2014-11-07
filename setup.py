0ca2a8434a6c8439f9d68696d0c1a1116c6b9d71
Raphaël Vinot <raphael@vinot.info>
2014-11-07 10:02:35 +0100

    Update to 1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='pypdns',
-    version='1.0',
+    version='1.1',
     author='Raphaël Vinot',
     author_email='raphael.vinot@circl.lu',
     maintainer='Raphaël Vinot',
