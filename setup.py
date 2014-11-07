e83bec3a2451c8c9f688538f4c1af8221adb537e
Raphaël Vinot <raphael@vinot.info>
2014-11-07 10:03:38 +0100

    Update to 1.1.1, fix pip install

--- setup.py
+++ setup.py
@@ -4,13 +4,12 @@ from setuptools import setup
 
 setup(
     name='pypdns',
-    version='1.1',
+    version='1.1.1',
     author='Raphaël Vinot',
     author_email='raphael.vinot@circl.lu',
     maintainer='Raphaël Vinot',
     url='https://github.com/CIRCL/PyPDNS',
     description='Python API for PDNS.',
-    long_description=open('README.md').read(),
     packages=['pypdns'],
     classifiers=[
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
