d59426ab1f9bc6badd70b60486c1a5527034a9de
juju4 <julien.t43+github@gmail.com>
2015-04-19 18:11:10 +0000

    add requests_cache option (default false) and logging

--- pypdns/api.py
+++ pypdns/api.py
@@ -4,6 +4,9 @@
 import datetime
 import json
 import requests
+import requests_cache
+import logging
+logger = logging
 
 sort_choice = ['count', 'rdata', 'rrname', 'rrtype', 'time_first', 'time_last']
 
@@ -11,10 +14,16 @@ sort_choice = ['count', 'rdata', 'rrname', 'rrtype', 'time_first', 'time_last']
 class PyPDNS(object):
 
     def __init__(self, url='https://www.circl.lu/pdns/query', basic_auth=None,
-                 auth_token=None):
+                 auth_token=None, enable_cache=False, cache_expire_after=604800, cache_file='/tmp/pdns.cache'):
         self.url = url
+        self.enable_cache = enable_cache
 
-        self.session = requests.Session()
+        if enable_cache is True:
+            requests_cache.install_cache()
+	    requests_cache.install_cache(cache_file, backend='sqlite', expire_after=cache_expire_after)
+            self.session = requests_cache.CachedSession()
+	else:
+            self.session = requests.Session()
         if basic_auth is not None:
             # basic_auth has do be a tuple ('user_name', 'password')
             self.session.auth = basic_auth
@@ -24,7 +33,9 @@ class PyPDNS(object):
             # No authentication defined.
             pass
 
+
     def query(self, q, sort_by='time_last'):
+        logger.info("start query() q=[%s]", q)
         if sort_by not in sort_choice:
             raise Exception('You can only sort by ' + ', '.join(sort_choice))
         response = self.session.get('{}/{}' .format(self.url, q))
@@ -33,8 +44,11 @@ class PyPDNS(object):
             if len(l) == 0:
                 continue
             try:
+                if self.enable_cache is True and response.from_cache is True:
+                    logger.info("from cache query() q=[%s]", q)
                 obj = json.loads(l)
             except:
+                logger.exception("except query() q=[%s]", q)
                 raise Exception('Unable to decode JSON object: ' + l)
             obj['time_first'] = datetime.datetime.fromtimestamp(obj['time_first'])
             obj['time_last'] = datetime.datetime.fromtimestamp(obj['time_last'])
