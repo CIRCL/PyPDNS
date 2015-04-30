3143c7948f3ff73c02359ca50a2fd2f5e59bca5b
Raphaël Vinot <raphael@vinot.info>
2015-04-30 11:59:09 +0200

    Fix mess up with the versioning

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='pypdns',
-    version='1.1.1',
+    version='1.2',
     author='Raphaël Vinot',
     author_email='raphael.vinot@circl.lu',
     maintainer='Raphaël Vinot',
