01efc6b7925c9ea6e81699a5e2fae6e338a99f69
Raphael Cohen <raphael.cohen@sekoia.fr>
2019-05-03 12:02:53 +0200

    feat: Improves http error handling

--- pypdns/api.py
+++ pypdns/api.py
@@ -4,6 +4,9 @@
 import datetime
 import json
 import requests
+
+from pypdns.errors import PDNSError, UnauthorizedError, ForbiddenError, RateLimitError, ServerError
+
 try:
     import requests_cache
     HAS_CACHE = True
@@ -21,7 +24,7 @@ class PyPDNS(object):
                  auth_token=None, enable_cache=False, cache_expire_after=604800, cache_file='/tmp/pdns.cache'):
         self.url = url
         if enable_cache and not HAS_CACHE:
-            raise Exception('Please install requests_cache if you want to use the caching capabilities.')
+            raise PDNSError('Please install requests_cache if you want to use the caching capabilities.')
         self.enable_cache = enable_cache
 
         if enable_cache is True:
@@ -41,10 +44,10 @@ class PyPDNS(object):
     def query(self, q, sort_by='time_last'):
         logger.info("start query() q=[%s]", q)
         if sort_by not in sort_choice:
-            raise Exception('You can only sort by ' + ', '.join(sort_choice))
+            raise PDNSError('You can only sort by ' + ', '.join(sort_choice))
         response = self.session.get('{}/{}' .format(self.url, q))
         if response.status_code != 200:
-            raise Exception('HTTP error authentication incorrect?')
+            self._handle_http_error(response)
         to_return = []
         for l in response.text.split('\n'):
             if len(l) == 0:
@@ -53,11 +56,23 @@ class PyPDNS(object):
                 if self.enable_cache is True and response.from_cache is True:
                     logger.info("from cache query() q=[%s]", q)
                 obj = json.loads(l)
-            except:
+            except Exception:
                 logger.exception("except query() q=[%s]", q)
-                raise Exception('Unable to decode JSON object: ' + l)
+                raise PDNSError('Unable to decode JSON object: ' + l)
             obj['time_first'] = datetime.datetime.fromtimestamp(obj['time_first'])
             obj['time_last'] = datetime.datetime.fromtimestamp(obj['time_last'])
             to_return.append(obj)
         to_return = sorted(to_return, key=lambda k: k[sort_by])
         return to_return
+
+    @staticmethod
+    def _handle_http_error(response):
+        if response.status_code == 401:
+            raise UnauthorizedError("Not authenticated: is authentication correct?")
+        if response.status_code == 403:
+            raise ForbiddenError("Not authorized to access resource")
+        if response.status_code == 429:
+            raise RateLimitError("Quota exhausted")
+        if 500 <= response.status_code < 600:
+            raise ServerError("Server error")
+        raise PDNSError("Something went wrong")
