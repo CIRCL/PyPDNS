91690f3f244ce8e618e3f76a7916aef03d3abaef
Raphaël Vinot <raphael@vinot.info>
2022-07-14 17:58:03 +0200

    chg: Improve type definitions.

--- pypdns/api.py
+++ pypdns/api.py
@@ -5,7 +5,8 @@ import json
 import logging
 
 from datetime import datetime
-from typing import Optional, Tuple, List, Dict, Union, Any
+from functools import cached_property
+from typing import Optional, Tuple, List, Dict, Union, Any, TypedDict
 
 import requests
 from requests import Session, Response
@@ -25,47 +26,138 @@ logger = logging.getLogger("pypdns")
 sort_choice = ['count', 'rdata', 'rrname', 'rrtype', 'time_first', 'time_last']
 
 
+class TypedPDNSRecord(TypedDict, total=False):
+
+    rrname: str
+    rrtype: str
+    rdata: Union[str, List[str]]
+    time_first: int
+    time_last: int
+    count: Optional[int]
+    bailiwick: Optional[str]
+    sensor_id: Optional[str]
+    zone_time_first: Optional[int]
+    zone_time_last: Optional[int]
+    origin: Optional[str]
+    time_first_ms: Optional[int]
+    time_last_ms: Optional[int]
+    time_first_rfc3339: Optional[str]
+    time_last_rfc3339: Optional[str]
+    meta: Optional[Dict[Any, Any]]
+
+
 class PDNSRecord:
 
-    def __init__(self, record: Dict[str, Optional[Union[str, int, bool, List[str]]]]):
-        self.record = record
+    def __init__(self, record: Dict[str, Optional[Union[str, int, bool, List[str], Dict[Any, Any]]]]):
+        self._record = record
 
     @property
-    def raw(self) -> Dict[str, Optional[Union[str, int, bool, List[str]]]]:
-        return self.record
+    def raw(self) -> Dict[str, Optional[Union[str, int, bool, List[str], Dict[Any, Any]]]]:
+        return self._record
+
+    @cached_property
+    def record(self) -> TypedPDNSRecord:
+        if not isinstance(self._record['rrname'], str):
+            raise PDNSRecordTypeError('rrname', 'str', self._record["rrname"])
+
+        if not isinstance(self._record['rrtype'], (str, int)):
+            raise PDNSRecordTypeError('rrtype', 'str, int', self._record["rrtype"])
+
+        if isinstance(self._record['rrtype'], int):
+            # Accordingly to the specs, the type can be a string OR an int. we normalize to str
+            rrtype: str = RdataType(self._record['rrtype']).name
+        else:
+            rrtype = RdataType[self._record['rrtype']].name
+
+        if not isinstance(self._record['rdata'], (str, list)):
+            raise PDNSRecordTypeError('rdata', 'str, list of string', self._record["rdata"])
+
+        if not isinstance(self._record['time_first'], int):
+            raise PDNSRecordTypeError('time_first', 'int', self._record["time_first"])
+
+        if not isinstance(self._record['time_last'], int):
+            raise PDNSRecordTypeError('time_last', 'int', self._record["time_last"])
+
+        to_return: TypedPDNSRecord = {'rrname': self._record['rrname'],
+                                      'rrtype': rrtype,
+                                      'rdata': self._record['rdata'],
+                                      'time_first': self._record["time_first"],
+                                      'time_last': self._record["time_last"]}
+        if 'count' in self._record:
+            if not isinstance(self._record['count'], int):
+                raise PDNSRecordTypeError('count', 'int', self._record["count"])
+            to_return['count'] = self._record["count"]
+
+        if 'bailiwick' in self._record:
+            if not isinstance(self._record['bailiwick'], str):
+                raise PDNSRecordTypeError('bailiwick', 'str', self._record["bailiwick"])
+            to_return['bailiwick'] = self._record['bailiwick']
+
+        if 'sensor_id' in self._record:
+            if not isinstance(self._record['sensor_id'], str):
+                raise PDNSRecordTypeError('sensor_id', 'str', self._record["sensor_id"])
+            to_return['sensor_id'] = self._record['sensor_id']
+
+        if 'zone_time_first' in self._record:
+            if not isinstance(self._record['zone_time_first'], int):
+                raise PDNSRecordTypeError('zone_time_first', 'int', self._record["zone_time_first"])
+            to_return['zone_time_first'] = self._record['zone_time_first']
+
+        if 'zone_time_last' in self._record:
+            if not isinstance(self._record['zone_time_last'], int):
+                raise PDNSRecordTypeError('zone_time_last', 'int', self._record["zone_time_last"])
+            to_return['zone_time_first'] = self._record["zone_time_last"]
+
+        if 'origin' in self._record:
+            if not isinstance(self._record['origin'], str):
+                raise PDNSRecordTypeError('origin', 'str', self._record["origin"])
+            to_return['origin'] = self._record["origin"]
+
+        if 'time_first_ms' in self._record:
+            if not isinstance(self._record['time_first_ms'], int):
+                raise PDNSRecordTypeError('time_first_ms', 'int', self._record["time_first_ms"])
+            to_return['time_first_ms'] = self._record["time_first_ms"]
+
+        if 'time_last_ms' in self._record:
+            if not isinstance(self._record['time_last_ms'], int):
+                raise PDNSRecordTypeError('time_last_ms', 'int', self._record["time_last_ms"])
+            to_return['time_last_ms'] = self._record['time_last_ms']
+
+        if 'time_first_rfc3339' in self._record:
+            if not isinstance(self._record['time_first_rfc3339'], str):
+                raise PDNSRecordTypeError('time_first_rfc3339', 'str', self._record["time_first_rfc3339"])
+            to_return['time_first_rfc3339'] = self._record['time_first_rfc3339']
+
+        if 'time_last_rfc3339' in self._record:
+            if not isinstance(self._record['time_last_rfc3339'], str):
+                raise PDNSRecordTypeError('time_last_rfc3339', 'str', self._record["time_last_rfc3339"])
+            to_return['time_last_rfc3339'] = self._record['time_last_rfc3339']
+
+        if 'meta' in self._record:
+            if not isinstance(self._record['meta'], dict):
+                raise PDNSRecordTypeError('meta', 'dict', self._record["meta"])
+            to_return['meta'] = self._record['meta']
+
+        return to_return
 
     @property
     def rrname(self) -> str:
-        if not isinstance(self.record['rrname'], str):
-            raise PDNSRecordTypeError('rrname', 'str', self.record["rrname"])
         return self.record['rrname']
 
     @property
     def rrtype(self) -> str:
-        if not isinstance(self.record['rrtype'], (str, int)):
-            raise PDNSRecordTypeError('rrtype', 'str, int', self.record["rrtype"])
-        if isinstance(self.record['rrtype'], int):
-            return RdataType(self.record['rrtype']).name
-        else:
-            # Just to make sure the type is correct
-            return RdataType[self.record['rrtype']].name
+        return self.record['rrtype']
 
     @property
     def rdata(self) -> Union[str, List[str]]:
-        if not isinstance(self.record['rdata'], (str, list)):
-            raise PDNSRecordTypeError('rrdata', 'str, list of string', self.record["rrdata"])
         return self.record['rdata']
 
     @property
     def time_first(self) -> int:
-        if not isinstance(self.record['time_first'], int):
-            raise PDNSRecordTypeError('time_first', 'int', self.record["time_first"])
         return self.record['time_first']
 
     @property
     def time_last(self) -> int:
-        if not isinstance(self.record['time_last'], int):
-            raise PDNSRecordTypeError('time_last', 'int', self.record["time_last"])
         return self.record['time_last']
 
     def __repr__(self) -> str:
@@ -81,91 +173,47 @@ class PDNSRecord:
 
     @property
     def count(self) -> Optional[int]:
-        if 'count' not in self.record:
-            return None
-        if not isinstance(self.record['count'], int):
-            raise PDNSRecordTypeError('count', 'int', self.record["count"])
-        return self.record['count']
+        return self.record.get('count')
 
     @property
     def bailiwick(self) -> Optional[str]:
-        if 'bailiwick' not in self.record:
-            return None
-        if not isinstance(self.record['bailiwick'], str):
-            raise PDNSRecordTypeError('bailiwick', 'str', self.record["bailiwick"])
-        return self.record['bailiwick']
+        return self.record.get('bailiwick')
 
     @property
     def sensor_id(self) -> Optional[str]:
-        if 'sensor_id' not in self.record:
-            return None
-        if not isinstance(self.record['sensor_id'], str):
-            raise PDNSRecordTypeError('sensor_id', 'str', self.record["sensor_id"])
-        return self.record['sensor_id']
+        return self.record.get('sensor_id')
 
     @property
     def zone_time_first(self) -> Optional[int]:
-        if 'zone_time_first' not in self.record:
-            return None
-        if not isinstance(self.record['zone_time_first'], int):
-            raise PDNSRecordTypeError('zone_time_first', 'int', self.record["zone_time_first"])
-        return self.record['zone_time_first']
+        return self.record.get('zone_time_first')
 
     @property
     def zone_time_last(self) -> Optional[int]:
-        if 'zone_time_last' not in self.record:
-            return None
-        if not isinstance(self.record['zone_time_last'], int):
-            raise PDNSRecordTypeError('zone_time_last', 'int', self.record["zone_time_last"])
-        return self.record['zone_time_last']
+        return self.record.get('zone_time_last')
 
     @property
     def origin(self) -> Optional[str]:
-        if 'origin' not in self.record:
-            return None
-        if not isinstance(self.record['origin'], str):
-            raise PDNSRecordTypeError('origin', 'str', self.record["origin"])
-        return self.record['origin']
+        return self.record.get('origin')
 
     @property
     def time_first_ms(self) -> Optional[int]:
-        if 'time_first_ms' not in self.record:
-            return None
-        if not isinstance(self.record['time_first_ms'], int):
-            raise PDNSRecordTypeError('time_first_ms', 'int', self.record["time_first_ms"])
-        return self.record['time_first_ms']
+        return self.record.get('time_first_ms')
 
     @property
     def time_last_ms(self) -> Optional[int]:
-        if 'time_last_ms' not in self.record:
-            return None
-        if not isinstance(self.record['time_last_ms'], int):
-            raise PDNSRecordTypeError('time_last_ms', 'int', self.record["time_last_ms"])
-        return self.record['time_last_ms']
+        return self.record.get('time_last_ms')
 
     @property
     def time_first_rfc3339(self) -> Optional[str]:
-        if 'time_first_rfc3339' not in self.record:
-            return None
-        if not isinstance(self.record['time_first_rfc3339'], str):
-            raise PDNSRecordTypeError('time_first_rfc3339', 'str', self.record["time_first_rfc3339"])
-        return self.record['time_first_rfc3339']
+        return self.record.get('time_first_rfc3339')
 
     @property
     def time_last_rfc3339(self) -> Optional[str]:
-        if 'time_last_rfc3339' not in self.record:
-            return None
-        if not isinstance(self.record['time_last_rfc3339'], str):
-            raise PDNSRecordTypeError('time_last_rfc3339', 'str', self.record["time_last_rfc3339"])
-        return self.record['time_last_rfc3339']
+        return self.record.get('time_last_rfc3339')
 
     @property
     def meta(self) -> Optional[Dict[Any, Any]]:
-        if 'meta' not in self.record:
-            return None
-        if not isinstance(self.record['meta'], dict):
-            raise PDNSRecordTypeError('meta', 'dict', self.record["meta"])
-        return self.record['meta']
+        return self.record.get('meta')
 
 
 class PyPDNS(object):
@@ -196,7 +244,7 @@ class PyPDNS(object):
             proxy = {'https': https_proxy_string}
             self.session.proxies.update(proxy)
 
-    def _query(self, q: str, sort_by: str = 'time_last', timeout: int = None) -> List[Dict[str, Optional[Union[str, int, bool, List[str]]]]]:
+    def _query(self, q: str, sort_by: str = 'time_last', timeout: int = None) -> List[Dict[str, Optional[Union[str, int, bool, List[str], Dict[Any, Any]]]]]:
         logger.debug("start query() q=[%s]", q)
         if sort_by not in sort_choice:
             raise PDNSError(f'You can only sort by {", ".join(sort_choice)}')
